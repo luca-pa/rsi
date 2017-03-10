83bdb73dd743def475329622dbf71f47092d76f5
luca-pa <luca.pa@live.it>
2017-03-10 12:01:56 +0100

    extracted models and repositories to be reused

---
+++ src/RSI.Models/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RSI.Models")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3fe5f9a2-3440-4e98-aae4-231a7cd374dc")]
---
+++ src/RSI.Models/Selezione.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RSI.Models
+{
+    public class Selezione
+    {
+        public string Ticker { get; set; }
+        public Etf Etf { get; set; }
+    }
+}
